a7c06195857f0b35e230a7d718f65adf7400812b
Teker <iamahaver@of.sex>
2024-07-08 14:40:49 -0600

    test-04 bugfix 03

--- femarch-1.sh
+++ femarch-1.sh
@@ -4,14 +4,14 @@ timedatectl
 mkfs.ext4 /dev/sda3
 mkswap /dev/sda2
 mkfs.fat -F 32 /dev/sda1
-mount /dev/sda1 /mnt/boot
+mount --mkdir /dev/sda1 /mnt/boot
 mount /dev/sda3 /mnt
 swapon /dev/sda2
 
 # pacstrap and chroot
 pacman-key --init
 pacman-key --populate
-pacstrap -K /mnt base linux linux-firmware vim networkmanager curl grub-install man
+pacstrap -K /mnt base linux linux-firmware vim networkmanager curl grub man
 genfstab -U /mnt >> /mnt/etc/fstab
 echo -en "\007"
 arch-chroot /mnt
