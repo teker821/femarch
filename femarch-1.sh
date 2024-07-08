bd0306e7d623ba6624a42d780e854b9abcb35da8
Teker <iamahaver@of.sex>
2024-07-08 14:37:56 -0600

    test-04 bugfix 01

--- femarch-1.sh
+++ femarch-1.sh
@@ -5,8 +5,8 @@ mkfs.ext4 /dev/sda3
 mkswap /dev/sda2
 mkfs.fat -F 32 /dev/sda1
 mkdir /mnt/boot
-mount /dev/sda3 /mnt
 mount /dev/sda1 /mnt/boot
+mount /dev/sda3 /mnt
 swapon /dev/sda2
 
 # pacstrap and chroot
